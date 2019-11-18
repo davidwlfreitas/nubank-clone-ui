67336b51d5c9f41963da2cd16bb7b48d7fef0efc
David Freitas <david.freitas@rfider.com>
2019-11-19 03:25:50 +1300

    feat: Added footer menu - Added library to generate QRCodes - Implemented main card along with list view behind the card

--- src/screens/home/index.vue
+++ src/screens/home/index.vue
@@ -1,84 +1,141 @@
 <template>
   <nb-container class="container">
-    <!-- <nb-header class="header"> 
-    </nb-header>-->
     <nb-content padder>
       <view class="intro">
-        <Image 
+        <Image
           class="logo"
           :source="require('../../../assets/Nubank_Logo.png')"
         />
         <nb-text class="title">
           David
         </nb-text>
-        <nb-icon name="ios-arrow-down" :style="{ fontSize: 14, color: '#FFF' }" />
+      </view>
+      <view class="arrow-down">
+        <nb-icon
+          name="ios-arrow-down"
+          :style="{ fontSize: 14, color: '#FFF' }"
+        />
       </view>
 
-      <scroll-view 
-        :ref="(scrollView) => { this.scrollView = scrollView; }"
+      <view class="content">
+        <view class="qr-code">
+          <QRCode
+            :value="qrCode"
+            :size="120"
+            :bgColor="bgColor"
+            :fgColor="fgColor"
+          />
+        </view>
+
+        <view class="menu-content">
+          <nb-list class="menu-list-items">
+            <nb-list-item></nb-list-item>
+            <nb-list-item>
+              <nb-icon
+                name="help-circle-outline"
+                :style="{ fontSize: 20, color: '#FFF' }"
+              />
+              <nb-body>
+                <nb-text class="tab-text">Me ajuda</nb-text>
+              </nb-body>
+            </nb-list-item>
+            <nb-list-item>
+              <nb-icon name="person" :style="{ fontSize: 20, color: '#FFF' }" />
+              <nb-body>
+                <nb-text class="tab-text">Perfil</nb-text>
+              </nb-body>
+            </nb-list-item>
+            <nb-list-item>
+              <nb-icon name="card" :style="{ fontSize: 20, color: '#FFF' }" />
+              <nb-body>
+                <nb-text class="tab-text">Configurar Cartão</nb-text>
+              </nb-body>
+            </nb-list-item>
+            <nb-list-item>
+              <nb-icon
+                name="phone-portrait"
+                :style="{ fontSize: 20, color: '#FFF' }"
+              />
+              <nb-body>
+                <nb-text class="tab-text">Configurações do app</nb-text>
+              </nb-body>
+            </nb-list-item>
+          </nb-list>
+        </view>
+
+        <nb-card class="content-card">
+          <nb-card-item class="card-header" header>
+            <nb-icon name="logo-usd" :style="{ fontSize: 28, color: '#666' }" />
+            <nb-icon name="eye-off" :style="{ fontSize: 28, color: '#666' }" />
+          </nb-card-item>
+          <nb-card-item class="card-content">
+            <nb-body>
+              <nb-text class="card-content-title">Saldo disponível</nb-text>
+              <nb-text class="card-content-description">
+                R$ 197.611,65
+              </nb-text>
+            </nb-body>
+          </nb-card-item>
+          <nb-card-item footer class="card-footer">
+            <nb-text class="card-footer-text"
+              >Tranferência de R$ 1.000 recebida de Mário Sérgio Cortella hoje
+              ás 08:00h</nb-text
+            >
+          </nb-card-item>
+        </nb-card>
+      </view>
+    </nb-content>
+
+    <nb-footer class="scroll-view-items">
+      <scroll-view
+        :ref="
+          scrollView => {
+            this.scrollView = scrollView;
+          }
+        "
+        :horizontal="true"
+        :showHorizontalScrollIndicator="false"
         :snap-to-interval="windowWidth - 60"
         :decelerationRate="0"
-        :horizontal="true"
-        :snap-to-alignment="'center'"
-        class="scroll-view-links"
-        >
-    
-      </scroll-view>
-
-     <!--  <view
-        :style="{
-          flex: 1,
-          flexDirection: 'row',
-          width: '100%',
-          justifyContent: 'center',
-        }"
       >
-        <view class="column-left">
-          <touchable-opacity
-            v-for="(brand, index) in brandsInfoLeft"
-            :key="index"
-            :on-press="() => setOrganization(brand)"
-          >
-            <view class="brand-shadow" :style="shadowStyle">
-              <Image
-                :source="{ uri: brand.download_url }"
-                class="brand-avatar"
-              />
-              <nb-text class="brand-title">
-                {{ brand.id }}
-              </nb-text>
-            </view>
-          </touchable-opacity>
+        <view class="tab-item">
+          <nb-icon name="person-add" :style="{ fontSize: 20, color: '#FFF' }" />
+          <nb-text class="tab-text">Indicar amigos</nb-text>
         </view>
-        <view class="column-middle" />
-        <view class="column-right">
-          <touchable-opacity
-            v-for="(brand, index) in brandsInfoRight"
-            :key="index"
-            :on-press="() => setOrganization(brand)"
-          >
-            <view class="brand-shadow" :style="shadowStyle">
-              <Image
-                :source="{ uri: brand.download_url }"
-                class="brand-avatar"
-              />
-              <nb-text class="brand-title">
-                {{ brand.id }}
-              </nb-text>
-            </view>
-          </touchable-opacity>
+        <view class="tab-item">
+          <nb-icon
+            name="chatbubbles"
+            :style="{ fontSize: 20, color: '#FFF' }"
+          />
+          <nb-text class="tab-text">Cobrar</nb-text>
         </view>
-      </view>
-      <nb-spinner v-if="loading" color="#0B70E4" class="loading" /> -->
-    </nb-content>
+        <view class="tab-item">
+          <nb-icon
+            name="arrow-round-down"
+            :style="{ fontSize: 20, color: '#FFF' }"
+          />
+          <nb-text class="tab-text">Depositar</nb-text>
+        </view>
+        <view class="tab-item">
+          <nb-icon
+            name="arrow-round-up"
+            :style="{ fontSize: 20, color: '#FFF' }"
+          />
+          <nb-text class="tab-text">Transferir</nb-text>
+        </view>
+        <view class="tab-item">
+          <nb-icon name="lock" :style="{ fontSize: 20, color: '#FFF' }" />
+          <nb-text class="tab-text">Bloquear cartão</nb-text>
+        </view>
+      </scroll-view>
+    </nb-footer>
   </nb-container>
 </template>
 
 <script>
-import Settings from '../../../settings';
-import store from '../../store';
-import { getStatusBarHeight } from 'react-native-iphone-x-helper'
-import { Linking, Dimensions, ScrollView, View, StyleSheet } from "react-native";
+import QRCode from 'react-native-qrcode';
+// import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { Dimensions } from 'react-native';
 export default {
   props: {
     navigation: {
@@ -88,136 +145,129 @@ export default {
   },
   data() {
     return {
-      brandsInfoLeft: [],
-      brandsInfoRight: [],
-      loading: false,
-      shadowStyle: {},
       // iPhoneXHeight: getStatusBarHeight,
-      windowWidth: Dimensions.get("screen").width,
-      windowHeight: Dimensions.get("screen").height
+      windowWidth: Dimensions.get('screen').width,
+      windowHeight: Dimensions.get('screen').height,
+      qrCode: 'https://nubank.com.br/en/',
+      bgColor: '#FFF',
+      fgColor: '#8b10ae',
     };
   },
-  mounted() {
-    this.getBrands();
-    this.shadowStyle = {
-      ...this.elevationShadowStyle(5),
-      backgroundColor: 'gray',
-    };
-  },
-  methods: {
-    getBrands() {
-      this.loading = true;
-      // let url = `${Settings.baseUrl}brands?clientId=${Settings.clientId}&clientSecret=${Settings.clientSecret}`;
-      let url = `${Settings.baseUrl}`;
-      fetch(url)
-        .then(response => response.json())
-        .then(responseJson => {
-          this.loading = false;
-          this.brandsInfoLeft = responseJson.filter(
-            (el, index) => index % 2 === 0,
-          );
-          this.brandsInfoRight = responseJson.filter(
-            (el, index) => index % 2 === 1,
-          );
-        })
-        .catch(error => console.log(error));
-    },
-    async setOrganization(brand) {
-      await store
-        .dispatch('SET_ORGANIZATION', {
-          organization: brand,
-        })
-        .then(this.navigation.navigate('Welcome', { brand: brand }));
-    },
-    elevationShadowStyle(elevation) {
-      return {
-        elevation: elevation,
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 0.1 * elevation },
-        shadowOpacity: 0.3,
-        shadowRadius: 0.8 * elevation,
-      };
-    },
+  components: {
+    QRCode,
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .container {
-  background-color: #8B10AE;
+  background-color: #8b10ae;
+  padding-top: 80px;
 }
 .header {
-  background-color: #8B10AE;
+  background-color: #8b10ae;
 }
-.intro{
+.intro {
   align-items: center;
   flex-direction: row;
   justify-content: center;
-  margin-bottom: 10px;
+  margin-bottom: 40px;
   width: 100%;
-  top: 0;
+  top: 20;
 }
 .title {
   font-size: 18px;
-  color: #FFF;
+  color: #fff;
   font-weight: bold;
   margin-left: 8px;
 }
-.scroll-view-links {
-  position: absolute;
-  bottom: 20;
-  width: 90%;
-  left: 5%;
-}
-.brand-text {
-  width: 60%;
-  margin-top: 20;
-  margin-bottom: 20;
-  margin-left: 20;
-}
-.brand-text1 {
-  font-size: 16;
-}
-.brand-text2 {
-  font-size: 48;
-}
-.brand-text3 {
-  font-size: 24;
-}
-.column-left {
-  width: 144;
-}
-.column-middle {
-  width: 5%;
-}
-.column-right {
-  width: 144;
-  padding-top: 97;
-}
-.brand-shadow {
-  flex: 1;
-  background-color: gray;
-  border-radius: 10;
-  margin-bottom: 20;
-}
-.brand-avatar {
-  width: 143;
-  height: 190;
-  position: relative;
-  border-radius: 10;
-  margin-bottom: 1;
-}
-.brand-title {
-  position: absolute;
-  left: 10;
-  bottom: 20;
-  color: white;
-  font-size: 24;
-}
-.loading {
+.arrow-down {
   align-items: center;
   justify-content: center;
   width: 100%;
   top: 0;
+}
+.scroll-view-items {
+  overflow: hidden;
+  bottom: 20;
+  height: 100px;
+  background-color: #8b10ae;
+  margin-left: 10px;
+}
+.tab-item {
+  width: 100px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 3px;
+  margin-left: 10px;
+  padding: 10px;
+  justify-content: space-between;
+}
+.tab-text {
+  color: #fff;
+  font-size: 13px;
+}
+.content {
+  flex: 1;
+  z-index: 5;
+  min-height: 400px;
+  align-items: center;
+}
+.content-card {
+  flex: 1;
+  border-radius: 4px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  margin-left: 10px;
+  margin-right: 10px;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 480px;
+}
+.card-header {
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 30px;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  padding-right: 30px;
+}
+.card-content {
+  flex: 1;
+  padding-left: 30px;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  padding-right: 30px;
+  justify-content: center;
+}
+.card-content-title {
+  font-size: 13px;
+  color: #999;
+}
+.card-content-description {
+  font-size: 32px;
+  margin-top: 3px;
+  color: #333;
+}
+.card-footer {
+  padding-left: 30px;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  padding-right: 30px;
+  background-color: #eee;
+  border-radius: 4px;
+}
+.card-footer-text {
+  font-size: 12px;
+  color: #333;
+}
+.menu-content {
+  width: 100%;
+}
+.qr-code {
+  padding: 20px;
 }
 </style>
